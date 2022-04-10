446a0c217714c3cddd92a0ce399e7b555a90c05e
Pavel <Pavel.Debchuk@gmail.com>
2022-04-10 21:48:37 +0300

    добавив запис контактів в localStorage, та рендер з нього

--- src/components/App.js
+++ src/components/App.js
@@ -50,6 +50,21 @@ class App extends Component {
     );
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   render() {
     const visibleContactCards = this.visibleContactCards();
     const { filter } = this.state;
